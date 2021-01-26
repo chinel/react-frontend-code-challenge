68e826876d1ce5bb1738c1f02f7e53838004d883
nelo <nelokwaz@gmail.com>
2021-01-26 22:22:51 +0100

    added the components to the component reference file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { default as DivisionSummary } from "./DivisionSummary/DivisionSummary";
+import { default as Header } from "./Header/Header";
+import { default as InfoBox } from "./InfoBox/InfoBox";
+import { default as ModuleHistory } from "./ModuleHistory/ModuleHistory";
+import { default as TableSummary } from "/TableSummary/TableSummary";
