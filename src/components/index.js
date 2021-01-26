695ececa65365e52c41d5225337814c0b5700e57
nelo <nelokwaz@gmail.com>
2021-01-26 22:27:44 +0100

    made some corrections to the component reference file

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import { default as DivisionSummary } from "./DivisionSummary/DivisionSummary";
-import { default as Header } from "./Header/Header";
-import { default as InfoBox } from "./InfoBox/InfoBox";
-import { default as ModuleHistory } from "./ModuleHistory/ModuleHistory";
-import { default as TableSummary } from "/TableSummary/TableSummary";
+export { default as DivisionSummary } from "./DivisionSummary/DivisionSummary";
+export { default as Header } from "./Header/Header";
+export { default as InfoBox } from "./InfoBox/InfoBox";
+export { default as ModuleHistory } from "./ModuleHistory/ModuleHistory";
+export { default as TableSummary } from "/TableSummary/TableSummary";
