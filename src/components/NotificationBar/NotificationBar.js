c5a67a6db362ca8bef95a1355ab8a8255850c72f
nelo <nelokwaz@gmail.com>
2021-01-29 15:49:49 +0100

    added the notification data from the database

--- src/components/NotificationBar/NotificationBar.js
+++ src/components/NotificationBar/NotificationBar.js
@@ -6,58 +6,55 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import WarningIcon from "@material-ui/icons/Warning";
 import useStyles from "./NotificationBarStyles";
+import { useQuery } from "@apollo/client";
+import { GET_NOTIFICATIONS } from "../../graphql-endpoint/queries";
+import LoadingProgress from "../LoadingProgress/LoadingProgress";
 
 export default function NotificationBar(props) {
   const { toggleDrawerHandler, status } = props;
   const classes = useStyles();
 
+  const { data, loading, error } = useQuery(GET_NOTIFICATIONS);
+
+  if (loading) {
+    return <LoadingProgress />;
+  }
+
+  if (error) return <div>Error fetching Notifications</div>;
+
   return (
-    <div>
-      <React.Fragment>
-        <Drawer
-          anchor="right"
-          open={status.right}
-          onClose={() => toggleDrawerHandler("right")}
-        >
-          <div
-            className={classes.list}
-            role="presentation"
-            onClick={() => toggleDrawerHandler("right")}
-            onKeyDown={() => toggleDrawerHandler("right")}
+    data.notifications && (
+      <div>
+        <React.Fragment>
+          <Drawer
+            anchor="right"
+            open={status.right}
+            onClose={() => toggleDrawerHandler("right")}
           >
-            <p className={classes.notificationHeader}>Notifications</p>
-            <List className={classes.mobileDropdownMenu}>
-              <ListItem button>
-                <ListItemIcon>
-                  <WarningIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Updates"
-                  secondary="Your account will expire on the 22nd"
-                />
-              </ListItem>
-              <ListItem button>
-                <ListItemIcon>
-                  <WarningIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Updates"
-                  secondary="Your account will expire on the 22nd"
-                />
-              </ListItem>
-              <ListItem button>
-                <ListItemIcon>
-                  <WarningIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary="Updates"
-                  secondary="Your account will expire on the 22nd"
-                />
-              </ListItem>
-            </List>
-          </div>
-        </Drawer>
-      </React.Fragment>
-    </div>
+            <div
+              className={classes.list}
+              role="presentation"
+              onClick={() => toggleDrawerHandler("right")}
+              onKeyDown={() => toggleDrawerHandler("right")}
+            >
+              <p className={classes.notificationHeader}>Notifications</p>
+              <List className={classes.mobileDropdownMenu}>
+                {data.notifications.map((notification) => (
+                  <ListItem button>
+                    <ListItemIcon>
+                      <WarningIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={notification.title}
+                      secondary={notification.description}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </Drawer>
+        </React.Fragment>
+      </div>
+    )
   );
 }
